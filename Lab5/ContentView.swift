8e1ef18ae858154848fcfab1705d600c4b41c347
Chernenko Illya <illyachernonko89@gmail.com>
2025-12-09 10:39:47 +0200

    Initial Commit

---
+++ Lab5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab5
+//
+//  Created by ІПЗ-31/3 on 12/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab5/Lab5App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab5App.swift
+//  Lab5
+//
+//  Created by ІПЗ-31/3 on 12/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Lab5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
